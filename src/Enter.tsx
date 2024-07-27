7682054a3de25636b45ac131cc2217a6d31a0604
niiroo <114127299+niiroo@users.noreply.github.com>
2024-07-27 14:35:32 +0900

    Enter add doorpoi

--- src/Enter.tsx
+++ src/Enter.tsx
@@ -1,13 +1,22 @@
-import { Tab, TabList, TabPanel, TabPanels, Tabs } from '@chakra-ui/react'
+import { Box, AbsoluteCenter } from '@chakra-ui/react'
 import './App.css'
 
-function App() {
+function Enter() {
 
     return (
         <>
-            <Tab>One</Tab>
+            <Box bg='rgb(232,208,179)' w='100%' h='100vh' p={4} color='black' >
+
+                <Box bg='rgb(213,183,147)' w='100%' h='100vh' p={4} color='black' >
+
+                    <AbsoluteCenter bg='white' w='50%' p={5} color='black' >
+                        Welcome to<br />
+                        ふぃ server room!
+                    </AbsoluteCenter >
+                </Box>
+            </Box>
         </>
     )
 }
 
-export default App
+export default Enter
