d7feca40295e9322e98350e1a0476c526900d5bc
niiroo <114127299+niiroo@users.noreply.github.com>
2024-07-27 23:09:16 +0900

    dog and cat

--- src/component/SE.tsx
+++ src/component/SE.tsx
@@ -14,6 +14,9 @@ import {
 } from "@chakra-ui/react";
 import { BellIcon } from "@chakra-ui/icons";
 import BombIcon from "../assets/bomb.svg"; // SVGファイルを直接インポート
+import DogIcon from "../assets/dog.svg"; // SVGファイルを直接インポート
+import CatIcon from "../assets/cat.svg"; // SVGファイルを直接インポート
+
 import useSound from 'use-sound';
 import BombSound from '../assets/爆発1.mp3';
 import DogSound from '../assets/犬の鳴き声1.mp3';
@@ -22,7 +25,7 @@ import CatSound from '../assets/猫の鳴き声1.mp3';
 
 function SE() {
 
-  const [bombPlay] = useSound(BombSound);
+  const [bombPlay] = useSound(BombSound,{ volume: 0.7 });
   const [dogPlay] = useSound(DogSound);
   const [catPlay] = useSound(CatSound);
 
@@ -50,8 +53,20 @@ function SE() {
                   colorScheme="blue"
                   leftIcon={<img src={BombIcon} alt="Bomb" />}
                   onClick={() => bombPlay()}>爆弾</Button>
-                <Button colorScheme="blue" onClick={() => dogPlay()}>いぬ</Button>
-                <Button colorScheme="blue" onClick={() => catPlay()}>ねこ</Button>
+                <Button colorScheme="blue">Button</Button>
+                <Button colorScheme="blue">Button</Button>
+              </HStack>
+              <HStack>
+                <Button colorScheme="blue">Button</Button>
+                <Button
+                  colorScheme="blue"
+                  leftIcon={<img src={DogIcon} alt="Dog" />}
+                  onClick={() => dogPlay()}>いぬ</Button>
+                <Button
+                  colorScheme="blue"
+                  leftIcon={<img src={CatIcon} alt="Cat" />}
+                  onClick={() => catPlay()}>ねこ</Button>
+
               </HStack>
               <HStack>
                 <Button colorScheme="blue">Button</Button>
