6893d7f9a1d35ad4ac7505454ed92286af2d19ab
uyuuuuu <soufflesumire.03@gmail.com>
2024-07-27 18:09:05 +0900

    roulette list can delete completely

--- src/Roulette.tsx
+++ src/Roulette.tsx
@@ -1,79 +1,28 @@
-import { DeleteIcon } from '@chakra-ui/icons';
 import {
     Box,
-    Button,
-    Editable,
-    EditableInput,
-    EditablePreview,
-    Flex,
-    VStack
+    Flex
 } from '@chakra-ui/react';
 import { useState } from 'react';
+import RouletteItemList from './component/RouletteItemList';
+import RouletteView from './component/RouletteView';
+
 
 function Roulette() {
     const [items, setItems] = useState<string[]>(['お題1', 'お題2', 'お題3', 'お題4', 'お題5']);
-    // 項目を追加ボタン -> お題◯っていうEditableが追加
-    function onClick() {
-        const newItemNumber = items.length + 1;
-        setItems([...items, `お題${newItemNumber}`]);
-        console.log(`新しい項目 "お題${newItemNumber}" が追加されました。`);
-    }
-    // 削除ボタン -> 項目を削除
-    function onDelete(index: number) {
-        const newItems = items.filter((_, i) => i !== index);
-        setItems(newItems);
-        for(let str of newItems){
-            console.log(str);
-        }
-        console.log(`index${index}のお題が削除されました。`);
-    }
-    // 項目のテキストが変更されたら -> コンソール出力(仮)
-    function onChange(index: number, newValue: string) {
-        const newItems = [...items];
-        newItems[index] = newValue;
-        setItems(newItems);
-        console.log(`value changed to "${newValue}".`);
-    }
+
     return (
         <Box width="1280px" height="800px">
             <Flex>
-                <VStack background={'teal.50'} w='50%' p={4}>
-                    ルーレットゾーン
-                </VStack>
-
-                <VStack background={'blue.50'} w='50%' p={4}>
-                    <div>お題一覧</div>
-                    {items.map((item, index) => (
-                        <Flex key={index} width="100%" alignItems="center">
-                            <Editable
-                                defaultValue={item}
-                                onChange={(newValue) => onChange(index, newValue)}
-                                flex="1"
-                                ml={9}
-                            >
-                                <EditablePreview
-                                    width="100%"
-                                    
-                                />
-                                <EditableInput
-                                    maxLength={30}
-                                    width={`${57}ch`}
-                                />
-                            </Editable>
-                            <Box textAlign="right" mr={9} >
-                                <Button
-                                    onClick={() => onDelete(index)}
-                                    colorScheme='blue' variant='outline' size='sm' p={1}>
-                                    <DeleteIcon boxSize={6} />
-                                </Button>
-                            </Box>
-                        </Flex>
-                    ))}
-                    {/* 追加ボタン */}
-                    <Button onClick={onClick} colorScheme='blue' variant='outline'>
-                        項目を追加
-                    </Button>
-                </VStack>
+                {/* ルーレット盤 */}
+                <RouletteView
+                    items={items}
+                    setItems={setItems}
+                />
+                {/* 項目リスト */}
+                <RouletteItemList
+                    items={items}
+                    setItems={setItems}
+                />
             </Flex>
         </Box>
     )
---
+++ src/component/RouletteItemList.tsx
@@ -0,0 +1,68 @@
+import { DeleteIcon } from "@chakra-ui/icons";
+import { Box, Button, Editable, EditableInput, EditablePreview, Flex, VStack } from "@chakra-ui/react";
+
+interface ItemListProps {
+    items: string[];
+    setItems: React.Dispatch<React.SetStateAction<string[]>>;
+}
+
+const RouletteView: React.FC<ItemListProps> = ({ items, setItems }) => {
+    // 項目を追加ボタン -> お題◯っていうEditableが追加
+    function onClick() {
+        const newItemNumber = items.length + 1;
+        setItems([...items, `お題${newItemNumber}`]);
+        console.log(`新しい項目 "お題${newItemNumber}" が追加されました。`);
+    }
+    // 削除ボタン -> 項目を削除
+    function onDelete(index: number) {
+        const newItems = items.filter((_, i) => i !== index);
+        setItems(newItems);
+        console.log(`index${index}のお題が削除されました。`);
+    }
+    // 項目のテキストが変更されたら -> コンソール出力(仮)
+    function onChange(index: number, newValue: string) {
+        const newItems = [...items];
+        newItems[index] = newValue;
+        setItems(newItems);
+        console.log(`value changed to "${newValue}".`);
+    }
+
+
+    return (
+        <VStack background={'blue.50'} w='50%' p={4}>
+            <div>お題一覧</div>
+            {items.map((item, index) => (
+                <Flex key={index} width="100%" alignItems="center">
+                    <Editable
+                        defaultValue={item}
+                        value={item}
+                        onChange={(newValue) => onChange(index, newValue)}
+                        flex="1"
+                        ml={9}
+                    >
+                        <EditablePreview
+                            width="100%"
+                        />
+                        <EditableInput
+                            maxLength={30}
+                            width={`${57}ch`}
+                        />
+                    </Editable>
+                    <Box textAlign="right" mr={9} >
+                        <Button
+                            onClick={() => onDelete(index)}
+                            colorScheme='blue' variant='outline' size='sm' p={1}>
+                            <DeleteIcon boxSize={6} />
+                        </Button>
+                    </Box>
+                </Flex>
+            ))}
+            {/* 追加ボタン */}
+            <Button onClick={onClick} colorScheme='blue' variant='outline'>
+                項目を追加
+            </Button>
+        </VStack>
+    )
+}
+
+export default RouletteView
