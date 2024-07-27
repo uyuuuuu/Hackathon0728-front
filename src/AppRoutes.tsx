0747129d5ea30a6fd5d242402200b1c53d49cbdd
uyuuuuu <soufflesumire.03@gmail.com>
2024-07-27 14:27:28 +0900

    update route

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -2,12 +2,14 @@
 import { Route, Routes } from "react-router-dom";
 import App from "./App";
 import Home from "./Home";
+import Roulette from "./Roulette";
 
 export const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/Home" element={<Home />} />
+      <Route path="/Roulette" element={<Roulette />} />
     </Routes>
   );
 };
