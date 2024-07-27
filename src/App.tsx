5344580d42e4a42e23671a5b06e6affc8b70592f
uyuuuuu <soufflesumire.03@gmail.com>
2024-07-27 11:59:24 +0900

    first

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import { Tab, TabList, TabPanel, TabPanels, Tabs } from '@chakra-ui/react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Tabs>
+        <TabList>
+          <Tab>One</Tab>
+          <Tab>Two</Tab>
+          <Tab>Three</Tab>
+        </TabList>
+
+        <TabPanels>
+          <TabPanel>
+            <p>one!</p>
+          </TabPanel>
+          <TabPanel>
+            <p>two!</p>
+          </TabPanel>
+          <TabPanel>
+            <p>three!</p>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+    </>
+  )
+}
+
+export default App
