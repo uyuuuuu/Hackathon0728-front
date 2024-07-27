b986f2c6736e87033756c3f4864129a9ccde2f13
22fi063 <22fi063@gmail.com>
2024-07-27 16:20:32 +0900

    component作成

--- src/component/Menu.tsx
+++ src/component/Menu.tsx
@@ -6,12 +6,12 @@ import {
   DrawerHeader,
   DrawerBody,
   Input,
-DrawerFooter,
-Button,
-useDisclosure
+  DrawerFooter,
+  Button,
+  useDisclosure,
 } from "@chakra-ui/react";
+import { HamburgerIcon } from "@chakra-ui/icons";
 import React from "react";
-
 
 function Menu() {
   const { isOpen, onOpen, onClose } = useDisclosure();
@@ -19,33 +19,33 @@ function Menu() {
 
   return (
     <>
-    <Button ref={btnRef} colorScheme='teal' onClick={onOpen}>
-      Open
-    </Button>
-    <Drawer
-      isOpen={isOpen}
-      placement='right'
-      onClose={onClose}
-      finalFocusRef={btnRef}
-    >
-      <DrawerOverlay />
-      <DrawerContent>
-        <DrawerCloseButton />
-        <DrawerHeader>Create your account</DrawerHeader>
-
-        <DrawerBody>
-          <Input placeholder='Type here...' />
-        </DrawerBody>
-
-        <DrawerFooter>
-          <Button variant='outline' mr={3} onClick={onClose}>
-            Cancel
-          </Button>
-          <Button colorScheme='blue'>Save</Button>
-        </DrawerFooter>
-      </DrawerContent>
-    </Drawer>
-  </>
+      <Button ref={btnRef} colorScheme="teal" onClick={onOpen} className="">
+        <HamburgerIcon />
+      </Button>
+      <Drawer
+        isOpen={isOpen}
+        placement="right"
+        onClose={onClose}
+        finalFocusRef={btnRef}
+      >
+        <DrawerOverlay />
+        <div className="h-[800px]">
+          <DrawerContent>
+            <DrawerCloseButton />
+            <DrawerHeader>Create your account</DrawerHeader>
+            <DrawerBody>
+              <Input placeholder="Type here..." />
+            </DrawerBody>
+            <DrawerFooter>
+              <Button variant="outline" mr={3} onClick={onClose}>
+                Cancel
+              </Button>
+              <Button colorScheme="blue">Save</Button>
+            </DrawerFooter>
+          </DrawerContent>
+        </div>
+      </Drawer>
+    </>
   );
 }
 
