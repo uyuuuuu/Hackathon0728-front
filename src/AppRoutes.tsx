aa0aee6ac0449ba776058e577011dc383f0d285b
niiroo <114127299+niiroo@users.noreply.github.com>
2024-07-27 15:33:46 +0900

    Enter hobo tukutta

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -2,12 +2,15 @@
 import { Route, Routes } from "react-router-dom";
 import App from "./App";
 import Home from "./Home";
+import Enter from "./Enter";
+
 
 export const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/Home" element={<Home />} />
+      <Route path="/Enter" element={<Enter />} />
     </Routes>
   );
 };
--- src/Enter.tsx
+++ src/Enter.tsx
@@ -1,20 +1,25 @@
-import { Box, AbsoluteCenter } from '@chakra-ui/react'
-import './App.css'
+import { Box, Center, Button } from '@chakra-ui/react'
+import { Link } from "react-router-dom";
 
 function Enter() {
 
     return (
         <>
-            <Box bg='rgb(232,208,179)' w='100%' h='100vh' p={4} color='black' >
+            <Center>
+                <Button bg='rgb(232,208,179)' w='60%' h='100vh' px={4} py={0} color='black' _hover={{ bg: '#E8D0B3' }} >
 
-                <Box bg='rgb(213,183,147)' w='100%' h='100vh' p={4} color='black' >
-
-                    <AbsoluteCenter bg='white' w='50%' p={5} color='black' >
-                        Welcome to<br />
-                        ふぃ server room!
-                    </AbsoluteCenter >
-                </Box>
-            </Box>
+                    <Box bg='rgb(213,183,147)' w='90%' h='100%' p={4} color='black' >
+                        <Center h='70%'>
+                            <Box bg='white' w='80%' p={5} color='black' fontSize='lg'>
+                                <Center>
+                                    Welcome to<br />
+                                    ふぃ server room!
+                                </Center>
+                            </Box >
+                        </Center>
+                    </Box>
+                </Button>
+            </Center>
         </>
     )
 }
