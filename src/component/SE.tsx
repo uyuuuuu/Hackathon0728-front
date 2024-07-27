bf20cd03ac1b93a8dbb9ed49fbd492b0dfd3c155
niiroo <114127299+niiroo@users.noreply.github.com>
2024-07-27 22:14:23 +0900

    add SE

--- src/component/SE.tsx
+++ src/component/SE.tsx
@@ -9,10 +9,24 @@ import {
   PopoverCloseButton,
   Button,
   Portal,
+  HStack,
+  VStack
 } from "@chakra-ui/react";
 import { BellIcon } from "@chakra-ui/icons";
+import BombIcon from "../assets/bomb.svg"; // SVGファイルを直接インポート
+import useSound from 'use-sound';
+import BombSound from '../assets/爆発1.mp3';
+import DogSound from '../assets/犬の鳴き声1.mp3';
+import CatSound from '../assets/猫の鳴き声1.mp3';
+
 
 function SE() {
+
+  const [bombPlay] = useSound(BombSound);
+  const [dogPlay] = useSound(DogSound);
+  const [catPlay] = useSound(CatSound);
+
+
   return (
     <Popover placement="top"> {/* ポップオーバーをボタンの上に表示 */}
       <PopoverTrigger>
@@ -27,12 +41,25 @@ function SE() {
       <Portal>
         <PopoverContent>
           <PopoverArrow />
-          <PopoverHeader>Header</PopoverHeader>
+          <PopoverHeader>SEを鳴らす</PopoverHeader>
           <PopoverCloseButton />
           <PopoverBody>
-            <Button colorScheme="blue">Button</Button>
+            <VStack p={2}>
+              <HStack>
+                <Button
+                  colorScheme="blue"
+                  leftIcon={<img src={BombIcon} alt="Bomb" />}
+                  onClick={() => bombPlay()}>爆弾</Button>
+                <Button colorScheme="blue" onClick={() => dogPlay()}>いぬ</Button>
+                <Button colorScheme="blue" onClick={() => catPlay()}>ねこ</Button>
+              </HStack>
+              <HStack>
+                <Button colorScheme="blue">Button</Button>
+                <Button colorScheme="blue">Button</Button>
+                <Button colorScheme="blue">Button</Button>
+              </HStack>
+            </VStack>
           </PopoverBody>
-          <PopoverFooter>This is the footer</PopoverFooter>
         </PopoverContent>
       </Portal>
     </Popover>
