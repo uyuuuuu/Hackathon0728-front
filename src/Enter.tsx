79825de0e67ef2505fada27d8cc8566294ae5440
niiroo <114127299+niiroo@users.noreply.github.com>
2024-07-27 15:52:26 +0900

    black sugiru nobu tuketa

--- src/Enter.tsx
+++ src/Enter.tsx
@@ -6,17 +6,21 @@ function Enter() {
     return (
         <>
             <Center>
-                <Button bg='rgb(232,208,179)' w='60%' h='100vh' px={4} py={0} color='black' _hover={{ bg: '#E8D0B3' }} >
+                
+                <Button as={Link} to="/Home" bg='rgb(232,208,179)' w='60%' h='100vh' px={4} py={0} color='black' _hover={{ bg: '#E8D0B3' }} >
 
                     <Box bg='rgb(213,183,147)' w='90%' h='100%' p={4} color='black' >
+                        
                         <Center h='70%'>
-                            <Box bg='white' w='80%' p={5} color='black' fontSize='lg'>
-                                <Center>
+                            <Box bg='white' w='80%' h='30%' p={5} color='black' fontSize='xl'>
+                                <Center h='100%'>
                                     Welcome to<br />
                                     ふぃ server room!
                                 </Center>
                             </Box >
                         </Center>
+
+                        <Box bg='black' w='80px' h='35px'p={4} color='black' rounded="full"></Box>
                     </Box>
                 </Button>
             </Center>
