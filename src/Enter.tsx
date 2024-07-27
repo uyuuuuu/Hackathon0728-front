f6b9e1b481aa04b338cf782f328bebc13b25b383
niiroo <114127299+niiroo@users.noreply.github.com>
2024-07-27 14:22:05 +0900

    途中っぽい

---
+++ src/Enter.tsx
@@ -0,0 +1,13 @@
+import { Tab, TabList, TabPanel, TabPanels, Tabs } from '@chakra-ui/react'
+import './App.css'
+
+function App() {
+
+    return (
+        <>
+            <Tab>One</Tab>
+        </>
+    )
+}
+
+export default App
